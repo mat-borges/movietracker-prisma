99a05cf75a533159bad7e28ce8fae8c0e2d63e7e
Mateus Borges <borgesm.work@gmail.com>
2023-01-26 18:51:23 -0300

    refactor: usersRepository to prisma

--- src/repositories/users-repositories/index.ts
+++ src/repositories/users-repositories/index.ts
@@ -1,13 +1,16 @@
-import { QueryResult } from "pg";
 import { User } from "protocols.js";
-import { connection } from "@/database/db";
+import prisma from "@/database/db.js";
 
-async function registerUser(name: string, email: string, password: string): Promise<QueryResult<User>> {
-  return connection.query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3)`, [name, email, password]);
+async function registerUser(name: string, email: string, password: string): Promise<User> {
+  return prisma.users.create({
+    data: { name, email, password },
+  });
 }
 
-async function checkEmail(email: string): Promise<QueryResult<User>> {
-  return connection.query(`SELECT * FROM users WHERE email=$1`, [email]);
+async function checkEmail(email: string): Promise<User | null> {
+  return prisma.users.findUnique({
+    where: { email },
+  });
 }
 
 const userRepository = {
