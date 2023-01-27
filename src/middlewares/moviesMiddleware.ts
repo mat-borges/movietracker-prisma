3e8dc26921fd97c2a8861aff5d489f45e165c232
Mateus Borges <borgesm.work@gmail.com>
2023-01-27 01:38:15 -0300

    refactor: movieMiddleware adapt to prisma client and import types

--- src/middlewares/moviesMiddleware.ts
+++ src/middlewares/moviesMiddleware.ts
@@ -1,23 +1,26 @@
 import { NextFunction, Request, Response } from "express";
 
+import { MovieBody } from "@/protocols";
 import { movieSchema } from "@/schemas";
 import { moviesRepository } from "@/repositories/movie-repositories";
 
 export function validateMovieSchema(req: Request, res: Response, next: NextFunction) {
-  const { title, description, year, genres, director, actors, poster } = req.body;
-  const movie = {
+  const { title, description, year, director, poster, actors, genres }: MovieBody = req.body;
+
+  const movie: MovieBody = {
     title: title.toLowerCase(),
     description,
     year: Number(year),
-    genres,
-    director,
-    actors,
+    director: director.toLowerCase(),
+    actors: actors.map((actor) => actor.toLowerCase()),
     poster,
+    genres: genres.map((genre) => genre.toLowerCase()),
   };
+
   const { error } = movieSchema.validate(movie, { abortEarly: false });
 
   if (error) {
-    const errors = error.details.map((detail: any) => detail.message);
+    const errors = error.details.map((detail) => detail.message);
     res.status(422).send({ errors });
   } else {
     res.locals.movie = movie;
@@ -27,9 +30,9 @@ export function validateMovieSchema(req: Request, res: Response, next: NextFunct
 
 export async function checkMovieExists(req: Request, res: Response, next: NextFunction) {
   const { title }: { title: string } = res.locals.movie;
-  const movieExists = (await moviesRepository.getMovieByTitle(title)).rows[0];
+  const movieExists = await moviesRepository.getMovieByTitle(title);
   if (movieExists) {
-    res.status(409).send({ error: `There's a movie with this title already (id:${movieExists.movie_id})` });
+    res.status(409).send({ error: `There's a movie with this title already (id:${movieExists.id})` });
   } else {
     next();
   }
