f9a78d927d04436af42ba88a0ebee318bf108c6b
Mateus Borges <borgesm.work@gmail.com>
2023-01-27 01:29:42 -0300

    refactor: remove types already generated by prisma client and add new movie types

--- src/protocols.ts
+++ src/protocols.ts
@@ -1,37 +1,20 @@
-export type User = {
-  id?: number;
-  name: string;
-  email: string;
-  password: string;
-  created_at?: Date;
+import { Movie } from "@prisma/client";
+
+export type MovieBody = Omit<Movie, "id" | "director_id"> & {
+  director: string;
+  actors: string[];
+  genres: string[];
 };
 
-export type Session = {
-  id?: number;
-  user_id: number;
-  token: string;
-  created_at: Date | string;
+export type MovieToDb = Omit<Movie, "id"> & {
+  director: string;
+  actors: { name: string }[];
+  genres: { name: string }[];
 };
 
-export type Movie = {
-  title: string;
-  description: string;
-  year: number;
-  poster: string;
-  director: number;
+export type MoviesFromDb = Omit<MovieBody, "actors" | "genres"> & {
+  id?: number;
+  director_id: number;
   genres: string[];
   actors: string[];
 };
-
-export type MovieFromDb = Omit<Movie, "director" | "genres" | "actors"> & {
-  id?: number;
-  // director: number;
-  // genres: number[];
-  // actors: number[];
-};
-
-export type Actor = { id?: number; name: string };
-
-export type Director = Actor;
-
-export type Genre = Actor;
